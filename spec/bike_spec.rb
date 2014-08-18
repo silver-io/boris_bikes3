6b518519f0813edd49533ebe14c9d3403a37feb3
javier <js@silveriofunk.com>
2014-08-18 17:31:43 +0100

    refactoring the test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,20 +1,19 @@
 require 'bike'
 
-describe Bike do 
+describe Bike do
+
+	let(:bike) { Bike.new }
 
 it "should not be broken after we create it" do
-	bike = Bike.new
 	expect(bike).not_to be_broken
 	end
 
 it "can break" do
-	bike = Bike.new
 	bike.break!
 	expect(bike).to be_broken
 	end
 
 it "can be fixed" do
-	bike = Bike.new
 	bike.break!
 	bike.fix!
 	expect(bike).not_to be_broken
